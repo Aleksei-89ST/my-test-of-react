4e5ee25087ff326001e0a9ada5231da62ba0d59b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-21 23:02:01 +0400

    component counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Counter = function () {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1 ) 
+  }
+  function dicrement() {
+    setCount(count - 1 )  
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>incement</button>
+      <button onClick={dicrement}>dicrement</button>
+    </div>
+  );
+};
+export default Counter
