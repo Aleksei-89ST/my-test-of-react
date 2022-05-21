04575febfe14d716ea157f5d93c2dc02d0b3dcc5
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-21 23:03:22 +0400

    logika app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+import MyButton from "./UI/button/MyButton";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1., title: "Java Script", body: "Description" },
+    { id: 2, title: "Java Script 2", body: "Description" },
+    { id: 3, title: "Java Script 3", body: "Description" },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type='text' placeholder="название поста"/>
+        <input type='text' placeholder="описание поста"/>
+        <MyButton>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Посты про JS' />
     </div>
   );
 }
