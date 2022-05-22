c500e6c6314a46b6b391f8ee2b405d68022ad1b6
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-22 23:41:41 +0400

    отработал callback - получил 2 переиспользуемых компонента

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useRef, useState } from "react";
+import PostForm from "./components/PostForm";
 // import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import "./styles/App.css";
-import MyButton from "./UI/button/MyButton";
-import MyInput from "./UI/input/MyInput";
+// import MyButton from "./UI/button/MyButton";
+// import MyInput from "./UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,41 +12,17 @@ function App() {
     { id: 2, title: "Java Script 2", body: "Description" },
     { id: 3, title: "Java Script 3", body: "Description" },
   ]);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+  // const [title, setTitle] = useState("");
+  // const [body, setBody] = useState("");
   //с помощью этого хука я получу доступ к дом элементу и уже этого дом элемента заберу value
   // const bodyInputRef = useRef(); // также с помощью useRef я создал ссылку - у этой ссылки есть единственное поле CURRENT - ЭТО И ЕСТЬ ДОМ ЭЛЕМЕНТ -у которо есть поле VALUE  и это value я смогу получить
-  const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body,
-    };
-  setPosts([...posts,newPost])
-  setTitle('')
-  setBody('')
-  };
+
   return (
     <div className="App">
-      <form>
-        {/*УПРАВЛЯЕМЫЙ КОМПОНЕНТ */}
-        <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          type="text"
-          placeholder="название поста"
-        />
-        {/*НЕУПРАВЛЯЕМЫЙ / НЕКОНТРОЛИРУЕМЫЙ КОМПОНЕНТ */}
-        {/* <input ref={bodyInputRef} type="text"/> */}
-        <MyInput
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          type="text"
-          placeholder="описание поста"
-        />
-        <MyButton onClick={addNewPost}> Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList posts={posts} title="Посты про JS" />
     </div>
   );
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import MyButton from "../UI/button/MyButton";
+import MyInput from "../UI/input/MyInput";
+
+function PostForm({create}) {
+  const [post, setPost] = useState({ title: "", body: "" });
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+        ...post , id: Date.now()
+    }
+    create(newPost)
+    setPost({ title: "", body: "" });
+  };
+  return (
+    <form>
+      {/*УПРАВЛЯЕМЫЙ КОМПОНЕНТ */}
+      <MyInput
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+        type="text"
+        placeholder="название поста"
+      />
+      {/*НЕУПРАВЛЯЕМЫЙ / НЕКОНТРОЛИРУЕМЫЙ КОМПОНЕНТ */}
+      {/* <input ref={bodyInputRef} type="text"/> */}
+      <MyInput
+        value={post.body}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
+        type="text"
+        placeholder="описание поста"
+      />
+      <MyButton onClick={addNewPost}> Создать пост</MyButton>
+    </form>
+  );
+}
+
+export default PostForm;
