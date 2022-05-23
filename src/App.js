53b75e439be48f6692566ca33983772272ff1365
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-23 14:00:16 +0400

    логика поиска с помощью useMemo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PostForm from "./components/PostForm";
 // import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import "./styles/App.css";
+import MyInput from "./UI/input/MyInput";
 import MySelect from "./UI/select/MySelect";
 // import MyButton from "./UI/button/MyButton";
 // import MyInput from "./UI/input/MyInput";
@@ -14,7 +15,23 @@ function App() {
     { id: 3, title: "vvvvvvvvv 3", body: "iiiiiiiiiiii" },
   ]);
   // реализую двухстороннее связывание
-  const [selectedSort,setSelectedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const sortedPosts = useMemo(() => {
+    console.log("ФУНКЦИЯ ОТРАБОТАЛА СОРТЕД ПОСТС");
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }, [selectedSort, posts]);
+  const sortedAndSearchedPost = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLocaleLowerCase().includes(searchQuery)
+    );
+  }, [searchQuery, sortedPosts]);
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -27,28 +44,36 @@ function App() {
   //с помощью этого хука я получу доступ к дом элементу и уже у этого дом элемента заберу value
   // const bodyInputRef = useRef(); // также с помощью useRef я создал ссылку - у этой ссылки есть единственное поле CURRENT - ЭТО И ЕСТЬ ДОМ ЭЛЕМЕНТ(в нашем случае) -у которо есть поле VALUE  и это value я смогу получить
 
-const sortPosts = (sort) => {
-  setSelectedSort(sort);
-  setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
-}
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+  };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <hr style={{margin: '15px 0'}}/>
+      <hr style={{ margin: "15px 0" }} />
       <div>
-      <MySelect 
-      value={selectedSort}
-      onChange={sortPosts}
-      defaultValue='Сортировка'
-      options={[
-        {value:'title', name:'По названию'},
-        {value:'body', name: 'По описанию'},
-      ]}
-      />
+        <MyInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Поиск..."
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
       </div>
-      {posts.length ? (
-        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      {sortedAndSearchedPost.length ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPost}
+          title="Посты про JS"
+        />
       ) : (
         <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
       )}
