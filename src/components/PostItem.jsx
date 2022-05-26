c4d00a4eb16ad6be9076c59b9dd9787649af234d
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-26 11:51:15 +0400

    исправил ошибку - связанную с выводом постов

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -14,7 +14,7 @@ const PostItem = (props) => {
         <div>{props.post.body}</div>
       </div>
       <div className="post__btns">
-        <MyButton onClick={() => navigate(`/posts/ ${props.post.id}`)}>Открыть</MyButton>
+        <MyButton onClick={() => navigate(`${props.post.id}`)}>Открыть</MyButton>
         <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
       </div>
     </div>
