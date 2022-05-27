ea3501a084e1e4df1477e29f4956ca592d0f6caa
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-27 15:17:31 +0400

    создал переиспользуемый компонент с роутами

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Error from '../pages/Error'
-import PostIdPage from '../pages/PostIdPage'
-import Posts from '../pages/Posts'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+// import About from "../pages/About";
+// import Error from "../pages/Error";
+// import Login from "../pages/Login";
+// import PostIdPage from "../pages/PostIdPage";
+// import Posts from "../pages/Posts";
+import { privateRoutes } from "../router/routes";
 
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts/:id" element={<PostIdPage />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-  )
-}
-
-export default AppRouter
+      {privateRoutes.map(route => 
+        <Route key={route} element={route.element} path={route.path} />
+        )}
+    {/* <Route path="/posts" element={<Posts />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/posts/:id" element={<PostIdPage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="*" element={<Error />} /> */}
+    <Route path="/" element={<Navigate to="/error" />} />
+  </Routes>
+  );
+};
+export default AppRouter;
