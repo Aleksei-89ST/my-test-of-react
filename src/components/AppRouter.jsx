bf550cf412d20c08e1af26e3e3ed5b90bc2f83ff
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-27 15:26:12 +0400

    создал форму для логинизации

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-// import About from "../pages/About";
-// import Error from "../pages/Error";
-// import Login from "../pages/Login";
-// import PostIdPage from "../pages/PostIdPage";
-// import Posts from "../pages/Posts";
-import { privateRoutes } from "../router/routes";
+import { privateRoutes, publicRoutes } from "../router/routes";
 
 const AppRouter = () => {
-  return (
+  const isAuth = true;
+  return isAuth ? (
     <Routes>
-      {privateRoutes.map(route => 
+      {privateRoutes.map((route) => (
         <Route key={route} element={route.element} path={route.path} />
-        )}
-    {/* <Route path="/posts" element={<Posts />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/posts/:id" element={<PostIdPage />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="*" element={<Error />} /> */}
-    <Route path="/" element={<Navigate to="/error" />} />
-  </Routes>
+      ))}
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
+  ) : (
+    <Routes>
+      {publicRoutes.map((route) => (
+        <Route key={route} element={route.element} path={route.path} />
+      ))}
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
   );
 };
 export default AppRouter;
